4ad59837d658dee66883182994b95706beb86826
Daniel Vázquez Pérez <danielvazquezperez5@gmail.com>
2023-02-07 12:48:15 +0100

    Actualizacion BBDD

--- proyectointegradordb.sql
+++ proyectointegradordb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-02-2023 a las 13:57:37
+-- Tiempo de generación: 07-02-2023 a las 11:54:01
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `chat` (
+  `id_chat` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `id_mensaje` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
   `id_vendedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -41,6 +41,7 @@ CREATE TABLE `chat` (
 --
 
 CREATE TABLE `compras` (
+  `id_compra` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `id_vendedor` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
@@ -51,8 +52,8 @@ CREATE TABLE `compras` (
 -- Volcado de datos para la tabla `compras`
 --
 
-INSERT INTO `compras` (`id_usuario`, `id_vendedor`, `id_producto`, `fecha_compra`) VALUES
-(2, 1, 3, 'hopy');
+INSERT INTO `compras` (`id_compra`, `id_usuario`, `id_vendedor`, `id_producto`, `fecha_compra`) VALUES
+(0, 2, 1, 3, 'hopy');
 
 -- --------------------------------------------------------
 
@@ -70,12 +71,12 @@ CREATE TABLE `generos` (
 --
 
 INSERT INTO `generos` (`tipo_genero`, `url_imagen`) VALUES
-('Aventura', ''),
-('Drama', ''),
-('Life Simulation', ''),
-('Puzzle', ''),
-('RPG', ''),
-('Shooter', '');
+('Acción', 'https://cdn-icons-png.flaticon.com/512/8027/8027925.png'),
+('Arcade', 'https://cdn-icons-png.flaticon.com/512/5846/5846307.png'),
+('Aventura', 'https://cdn-icons-png.flaticon.com/512/2790/2790402.png'),
+('Deportes', 'https://cdn1.iconfinder.com/data/icons/game-design-butterscotch-vol-2/256/Sports_Game-1024.png'),
+('Puzzle', 'https://cdn-icons-png.flaticon.com/512/4205/4205637.png'),
+('RPG', 'https://cdn-icons-png.flaticon.com/512/3426/3426247.png');
 
 -- --------------------------------------------------------
 
@@ -120,7 +121,8 @@ CREATE TABLE `juegos_favoritos` (
 
 CREATE TABLE `mensaje` (
   `id_mensaje` int(11) NOT NULL,
-  `contenido_mensaje` varchar(110) NOT NULL
+  `contenido_mensaje` varchar(110) NOT NULL,
+  `id_chat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -149,7 +151,8 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellidos`, `mail`, `contrasena`, `direccion`, `codigo_postal`, `ciudad`, `fecha_nacimiento`, `saldo_monedero`) VALUES
 (1, 'Juan', 'Munoz', 'pruab1@prueba.com', '12345678', 'avd. la mena', '2342', 'madrid', '12 diciembre 2023', '10.25'),
 (2, 'Monica', 'Naranko', 'monica@prueba.com', '12345678', 'avd. la mena', '12332', 'madrid', '12 diciembre 2023', '0.00'),
-(3, 'Juan', 'Perez', 'juanperez@gmail.com', '123456', 'Calle falsa', '28172', 'Madrid', '18 de enero de 1999', '0.00');
+(3, 'Juan', 'Perez', 'juanperez@gmail.com', '123456', 'Calle falsa', '28172', 'Madrid', '18 de enero de 1999', '0.00'),
+(4, 'Juan', 'Perez', 'juanperez@gmail.com', '123456', 'Calle falsa', '28172', 'Madrid', '18 de enero de 1999', '0.00');
 
 -- --------------------------------------------------------
 
@@ -171,7 +174,7 @@ CREATE TABLE `usuarios_favoritos` (
 -- Indices de la tabla `chat`
 --
 ALTER TABLE `chat`
-  ADD PRIMARY KEY (`id_mensaje`),
+  ADD PRIMARY KEY (`id_chat`),
   ADD KEY `id_producto` (`id_producto`),
   ADD KEY `id_usuario` (`id_usuario`),
   ADD KEY `id_vendedor` (`id_vendedor`);
@@ -180,6 +183,7 @@ ALTER TABLE `chat`
 -- Indices de la tabla `compras`
 --
 ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id_compra`),
   ADD KEY `id_producto` (`id_producto`),
   ADD KEY `id_usuario` (`id_usuario`),
   ADD KEY `id_vendedor` (`id_vendedor`);
@@ -210,7 +214,8 @@ ALTER TABLE `juegos_favoritos`
 -- Indices de la tabla `mensaje`
 --
 ALTER TABLE `mensaje`
-  ADD KEY `id_mensaje` (`id_mensaje`);
+  ADD PRIMARY KEY (`id_mensaje`),
+  ADD KEY `id_chat` (`id_chat`);
 
 --
 -- Indices de la tabla `usuario`
@@ -222,6 +227,7 @@ ALTER TABLE `usuario`
 -- Indices de la tabla `usuarios_favoritos`
 --
 ALTER TABLE `usuarios_favoritos`
+  ADD PRIMARY KEY (`id_usuario`,`id_vendedor`),
   ADD KEY `id_usuario` (`id_usuario`),
   ADD KEY `id_vendedor` (`id_vendedor`);
 
@@ -230,22 +236,22 @@ ALTER TABLE `usuarios_favoritos`
 --
 
 --
--- AUTO_INCREMENT de la tabla `chat`
---
-ALTER TABLE `chat`
-  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `juegos`
 --
 ALTER TABLE `juegos`
   MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `mensaje`
+--
+ALTER TABLE `mensaje`
+  MODIFY `id_mensaje` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -285,7 +291,7 @@ ALTER TABLE `juegos_favoritos`
 -- Filtros para la tabla `mensaje`
 --
 ALTER TABLE `mensaje`
-  ADD CONSTRAINT `mensaje_ibfk_1` FOREIGN KEY (`id_mensaje`) REFERENCES `chat` (`id_mensaje`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `id_chat` FOREIGN KEY (`id_chat`) REFERENCES `chat` (`id_chat`);
 
 --
 -- Filtros para la tabla `usuarios_favoritos`
